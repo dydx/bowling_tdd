3516383fb11b0736cd0ba1e26dfb35f065ae9afc
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-26 05:28:49 -0400

    extracted some methods from the if block

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -8,25 +8,43 @@ class Bowling
     @rolls << pins
   end
 
-  def is_spare?(rollCount)
-    (@rolls[rollCount] + @rolls[rollCount + 1] == 10)
-  end
-
   def score
     score = 0
     rollCount = 0
     (0...10).each do
-      if (@rolls[rollCount] == 10)
-        score += (10 + @rolls[rollCount + 1] + @rolls[rollCount + 2])
+      if is_strike?(rollCount)
+        score += 10 + next_two_balls_for_strike(rollCount)
         rollCount += 1
       elsif is_spare?(rollCount)
-        score += (10 + @rolls[rollCount + 2])
+        score += 10 + next_ball_for_spare(rollCount)
         rollCount += 2
       else
-        score += (@rolls[rollCount] + @rolls[rollCount + 1])
+        score += two_balls_in_frame(rollCount)
         rollCount += 2
       end
     end
     score
+  end
+
+private
+
+  def two_balls_in_frame(rollCount)
+    @rolls[rollCount] + @rolls[rollCount + 1]
+  end
+
+  def is_spare?(rollCount)
+    (@rolls[rollCount] + @rolls[rollCount + 1] == 10)
+  end
+
+  def next_ball_for_spare(rollCount)
+    @rolls[rollCount + 2]
+  end
+
+  def is_strike?(rollCount)
+    @rolls[rollCount] == 10
+  end
+
+  def next_two_balls_for_strike(rollCount)
+    @rolls[rollCount + 1] + @rolls[rollCount + 2]
   end
 end
