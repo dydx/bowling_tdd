f051dfd41c57e6dda7f9fb43ce6bba485d0d668b
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-26 05:21:45 -0400

    added test for a single spare and a single strike

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,15 +1,32 @@
 class Bowling
 
   def initialize
-    @score = 0
+    @rolls = []
   end
 
   def roll(pins)
-    @score += pins
+    @rolls << pins
+  end
+
+  def is_spare?(rollCount)
+    (@rolls[rollCount] + @rolls[rollCount + 1] == 10)
   end
 
   def score
-    @score
+    score = 0
+    rollCount = 0
+    (0...10).each do
+      if (@rolls[rollCount] == 10)
+        score += (10 + @rolls[rollCount + 1] + @rolls[rollCount + 2])
+        rollCount += 1
+      elsif is_spare?(rollCount)
+        score += (10 + @rolls[rollCount + 2])
+        rollCount += 2
+      else
+        score += (@rolls[rollCount] + @rolls[rollCount + 1])
+        rollCount += 2
+      end
+    end
+    score
   end
-
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,6 +2,16 @@ require_relative '../lib/bowling'
 
 describe Bowling do
 
+  def roll_many(n, pins)
+    n.times do
+      @game.roll(pins)
+    end
+  end
+
+  def roll_spare
+    roll_many(2, 5)
+  end
+
   before(:each) do
     @game = Bowling.new
   end
@@ -18,21 +28,42 @@ describe Bowling do
       expect(@game).to receive(:roll).with(0)
       @game.roll(0)
     end
+  end
 
+  describe ".score" do
     it "has a zero score after rolling all gutterballs" do
-      20.times do
-        @game.roll(0)
-      end
+      roll_many(20, 0)
       expect(@game.score).to eql(0)
       @game.score
     end
 
     it "has a score of 20 after rolling all ones" do
-      20.times do
-        @game.roll(1)
-      end
+      roll_many(20, 1)
       expect(@game.score).to eql(20)
       @game.score
     end
+
+  end
+
+  describe "one spare" do
+
+    it "calculates the score with one spare" do
+      roll_spare
+      @game.roll(3)
+      roll_many(17, 0)
+      expect(@game.score).to eql(16)
+    end
+  end
+
+  describe "one strike" do
+
+    it "calculates the score with one strike" do
+      @game.roll(10)
+      @game.roll(3)
+      @game.roll(4)
+      roll_many(16, 0)
+      expect(@game.score).to eql(24)
+    end
+
   end
 end
